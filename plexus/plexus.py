91f5f1894e3b8203bfec258bea7ac0184ba070af
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-03-07 05:21:27 +0300

    calculate_potential() function improved.

--- plexus/plexus.py
+++ plexus/plexus.py
@@ -8,6 +8,7 @@ import sys
 import ctypes
 import math
 import threading
+import gc
 
 POTENTIAL_RANGE = 110000 # Resting potential: -70 mV Membrane potential range: +40 mV to -70 mV --- Difference: 110 mV = 110000 microVolt --- https://en.wikipedia.org/wiki/Membrane_potential
 ACTION_POTENTIAL = 15000 # Resting potential: -70 mV Action potential: -55 mV --- Difference: 15mV = 15000 microVolt --- https://faculty.washington.edu/chudler/ap.html
@@ -23,6 +24,7 @@ class Neuron():
 		self.instability = 0.0
 		self.type = 0
 		network.neurons.append(self)
+		self.network = network
 
 	def fully_subscribe(self,network):
 		for neuron in network.neurons[len(self.subscriptions):]:
@@ -38,20 +40,24 @@ class Neuron():
 					self.subscriptions[id(neuron)] = round(random.uniform(0.1, 1.0), 2)
 			network.initiated_neurons += 1
 
-	def get_neuron(self,id):
-		return ctypes.cast(id, ctypes.py_object).potential
+	def get_neuron(self,neuron_id):
+		#return ctypes.cast(id, ctypes.py_object)
+		for neuron in self.network.neurons:
+			if id(neuron) == neuron_id:
+				return neuron
+		raise Exception("No found")
 
-	def primitive_calculate(self):
-		grand_total = 0
-		for neuron_id in self.subscriptions:
-			grand_total += self.get_neuron(neuron_id).potential * (1 / self.subscriptions[neuron_id])
-		print grand_total
-		print self.activation_function(grand_total)
+	def calculate_potential(self):
+		total = 0
+		for neuron_id, weight in self.subscriptions.iteritems():
+			total += self.get_neuron(neuron_id).potential * weight
+		return self.activation_function(total)
 
 	def activation_function(self,value):
 		return abs(math.sin(value**2))
 
 	def fire(self):
+		self.potential = self.calculate_potential()
 		self.instability = round(random.uniform(0.1, 1.0), 2)
 
 class Network():
