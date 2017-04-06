c28bb68360a3b166cec8b8c31e2c7a510fafb190
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-04-06 12:52:44 +0300

    Fix the catdog example

--- examples/catdog.py
+++ examples/catdog.py
@@ -8,20 +8,20 @@ from itertools import repeat
 import sys
 import time
 
-SIZE = 32 * 32 * 3 + 3 + 32 * 3
+SIZE = 32 * 32 * 3 + 3 + 256
 INPUT_SIZE = 32 * 32 * 3
 OUTPUT_SIZE = 3
-CONNECTIVITY = 0.1
+CONNECTIVITY = 0.05
 PRECISION = 3
 
 TRAINING_DURATION = 3
 RANDOMLY_FIRE = False
 DYNAMIC_OUTPUT = False
 
-TRAINING_SAMPLE_SIZE = 100
-TESTING_SAMPLE_SIZE = 100
+TRAINING_SAMPLE_SIZE = 80
+TESTING_SAMPLE_SIZE = 20
 
-DOMINANCE_THRESHOLD = 0.3
+DOMINANCE_THRESHOLD = 0.7
 error = 0
 error_divisor = 0
 
@@ -106,8 +106,6 @@ for i in range(0, num_train_samples - 1):
     if y_train[i] == 5:
         dogs.append(x_train[i])
 
-cats_sample = random.sample(cats, TRAINING_SAMPLE_SIZE)
-dogs_sample = random.sample(dogs, TRAINING_SAMPLE_SIZE)
 
 # Generate the testing data
 test_cats = []
@@ -120,8 +118,6 @@ for i in range(0, num_train_samples/5 - 1):
     if y_test[i] == 5:
         test_dogs.append(x_test[i])
 
-test_cats_sample = random.sample(test_cats, TESTING_SAMPLE_SIZE)
-test_dogs_sample = random.sample(test_dogs, TESTING_SAMPLE_SIZE)
 
 blue = np.array([255, 0, 0])
 red = np.array([0, 0, 255])
@@ -133,35 +129,38 @@ net = plexus.Network(SIZE,INPUT_SIZE,OUTPUT_SIZE,CONNECTIVITY,PRECISION,RANDOMLY
 
 print "\n*** LEARNING ***"
 
-print "\nMap " + str(TRAINING_SAMPLE_SIZE) + " Different Cat Images to Color Blue - Training Duration: " + str(TRAINING_DURATION * TRAINING_SAMPLE_SIZE) + " seconds"
-for cat in cats_sample:
-    cat_normalized = np.true_divide(cat, 255).flatten()
-    blue_normalized = np.true_divide(blue, 255).flatten()
-    cv2.imshow("Input", cat)
-    net.load(cat_normalized,blue_normalized)
+print "\nMap " + str(TRAINING_SAMPLE_SIZE/2) + " Different Cat Images to Color Blue & " + str(TRAINING_SAMPLE_SIZE/2) + " Different Dog Images to Color Red - Training Duration: " + str(TRAINING_DURATION * TRAINING_SAMPLE_SIZE) + " seconds (OpenCV latency not included)"
+for i in range(1,TRAINING_SAMPLE_SIZE):
+    if (i % 2) == 0:
+        cat = random.sample(cats, 1)[0]
+        cat_normalized = np.true_divide(cat, 255).flatten()
+        blue_normalized = np.true_divide(blue, 255).flatten()
+        cv2.imshow("Input", cat)
+        net.load(cat_normalized,blue_normalized)
+    else:
+        dog = random.sample(dogs, 1)[0]
+        dog_normalized = np.true_divide(dog, 255).flatten()
+        red_normalized = np.true_divide(red, 255).flatten()
+        cv2.imshow("Input", dog)
+        net.load(dog_normalized,red_normalized)
     show_output(net)
 
-print "\nMap " + str(TRAINING_SAMPLE_SIZE) + " Different Dog Images to Color Red - Training Duration: " + str(TRAINING_DURATION * TRAINING_SAMPLE_SIZE) + " seconds"
-for dog in dogs_sample:
-    dog_normalized = np.true_divide(dog, 255).flatten()
-    red_normalized = np.true_divide(red, 255).flatten()
-    cv2.imshow("Input", dog)
-    net.load(dog_normalized,red_normalized)
-    show_output(net)
 
-print "\nTest " + str(TESTING_SAMPLE_SIZE) + " Different Cat Images - Testing Duration: " + str(TRAINING_DURATION * TESTING_SAMPLE_SIZE) + " seconds"
-for cat in test_cats_sample:
-    cat_normalized = np.true_divide(cat, 255).flatten()
-    cv2.imshow("Input", cat)
-    net.load(cat_normalized)
-    show_output(net,[1.0, 0.0, 0.0])
-
-print "\nTest " + str(TESTING_SAMPLE_SIZE) + " Different Dog Images - Testing Duration: " + str(TRAINING_DURATION * TESTING_SAMPLE_SIZE) + " seconds"
-for dog in test_dogs_sample:
-    dog_normalized = np.true_divide(dog, 255).flatten()
-    cv2.imshow("Input", dog)
-    net.load(dog_normalized)
-    show_output(net,[0.0, 0.0, 1.0])
+print "\nTest " + str(TESTING_SAMPLE_SIZE/2) + " Different Cat Images & " + str(TESTING_SAMPLE_SIZE/2) + " Different Dog Images - Testing Duration: " + str(TRAINING_DURATION * TESTING_SAMPLE_SIZE) + " seconds (OpenCV latency not included)"
+for i in range(1,TESTING_SAMPLE_SIZE):
+    binary_random = random.randint(0,1)
+    if binary_random == 0:
+        cat = random.sample(test_cats, 1)[0]
+        cat_normalized = np.true_divide(cat, 255).flatten()
+        cv2.imshow("Input", cat)
+        net.load(cat_normalized)
+        show_output(net,[1.0, 0.0, 0.0])
+    else:
+        dog = random.sample(test_dogs, 1)[0]
+        dog_normalized = np.true_divide(dog, 255).flatten()
+        cv2.imshow("Input", dog)
+        net.load(dog_normalized)
+        show_output(net,[0.0, 0.0, 1.0])
 
 
 net.freeze()
