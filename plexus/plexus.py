8e6aa906ca93677644fcc923d8c56ab3a2dd554d
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2017-09-02 10:01:28 +0300

    Add a function for the visualization of the network

--- plexus/plexus.py
+++ plexus/plexus.py
@@ -126,6 +126,7 @@ class Network():
 
 		self.freezer = False
 		self.thread = None
+		self.visualize()
 		self.ignite()
 
 		print ""
@@ -303,3 +304,37 @@ class Network():
 		for neuron in self.motor_neurons:
 			output.append( round(neuron.potential, self.precision) )
 		return output
+
+	def visualize(self):
+		thread = threading.Thread(target=self._visualize)
+		thread.start()
+		print "Visualization initiated"
+
+	def _visualize(self):
+		from pyqtgraph.Qt import QtGui, QtCore
+		import pyqtgraph as pg
+
+		app = QtGui.QApplication([])
+		win = pg.GraphicsWindow(title="Scatter Plot Symbols")
+		win.resize(1000,600)
+
+		pg.setConfigOptions(antialias=True)
+
+		plot = win.addPlot(title="Plotting with symbols")
+		plot.addLegend()
+		plot.plot([0, 1, 2, 3, 4], pen=(0,0,200), symbolBrush=(0,0,200), symbolPen='w', symbol='o', symbolSize=14, name="symbol='o'")
+		plot.plot([1, 2, 3, 4, 5], pen=(0,128,0), symbolBrush=(0,128,0), symbolPen='w', symbol='t', symbolSize=14, name="symbol='t'")
+		plot.plot([2, 3, 4, 5, 6], pen=(19,234,201), symbolBrush=(19,234,201), symbolPen='w', symbol='t1', symbolSize=14, name="symbol='t1'")
+		plot.plot([3, 4, 5, 6, 7], pen=(195,46,212), symbolBrush=(195,46,212), symbolPen='w', symbol='t2', symbolSize=14, name="symbol='t2'")
+		plot.plot([4, 5, 6, 7, 8], pen=(250,194,5), symbolBrush=(250,194,5), symbolPen='w', symbol='t3', symbolSize=14, name="symbol='t3'")
+		plot.plot([5, 6, 7, 8, 9], pen=(54,55,55), symbolBrush=(55,55,55), symbolPen='w', symbol='s', symbolSize=14, name="symbol='s'")
+		plot.plot([6, 7, 8, 9, 10], pen=(0,114,189), symbolBrush=(0,114,189), symbolPen='w', symbol='p', symbolSize=14, name="symbol='p'")
+		plot.plot([7, 8, 9, 10, 11], pen=(217,83,25), symbolBrush=(217,83,25), symbolPen='w', symbol='h', symbolSize=14, name="symbol='h'")
+		plot.plot([8, 9, 10, 11, 12], pen=(237,177,32), symbolBrush=(237,177,32), symbolPen='w', symbol='star', symbolSize=14, name="symbol='star'")
+		plot.plot([9, 10, 11, 12, 13], pen=(126,47,142), symbolBrush=(126,47,142), symbolPen='w', symbol='+', symbolSize=14, name="symbol='+'")
+		plot.plot([10, 11, 12, 13, 14], pen=(119,172,48), symbolBrush=(119,172,48), symbolPen='w', symbol='d', symbolSize=14, name="symbol='d'")
+		plot.setXRange(-2, 4)
+
+		import sys
+		if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+			QtGui.QApplication.instance().exec_()
