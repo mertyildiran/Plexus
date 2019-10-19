d7600309940028ab4b0ca54538b58bec9d06affc
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2019-10-19 05:57:33 +0300

    Automatically download CIFAR-10 dataset at beginning of the Cat-Dog example and fix the linting issues in the other examples

--- examples/catdog.py
+++ examples/catdog.py
@@ -4,9 +4,35 @@ import os
 import cv2
 import random
 import plexus
-from itertools import repeat
-import sys
+# import sys
 import time
+import tarfile
+from tqdm import tqdm
+import requests
+
+
+basepath = os.path.dirname(os.path.realpath(__file__))
+
+if not os.path.exists(basepath + '/cifar-10-python.tar.gz'):
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    r = requests.get(url, stream=True)
+    total_size = int(r.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    t = tqdm(total=total_size, unit='iB', unit_scale=True)
+    with open(basepath + '/cifar-10-python.tar.gz', 'wb') as f:
+        for data in r.iter_content(block_size):
+            t.update(len(data))
+            f.write(data)
+    t.close()
+    if total_size != 0 and t.n != total_size:
+        print("ERROR, something went wrong")
+        exit(1)
+
+if not os.path.exists(basepath + '/cifar-10-batches-py/'):
+    tar = tarfile.open(basepath + '/cifar-10-python.tar.gz', "r:gz")
+    tar.extractall(basepath)
+    tar.close()
+
 
 SIZE = 32 * 32 * 3 + 3 + 256
 INPUT_SIZE = 32 * 32 * 3
@@ -26,10 +52,11 @@ DOMINANCE_THRESHOLD = 0.7
 error = 0
 error_divisor = 0
 
+
 def load_batch(fpath, label_key='labels'):
     # Internal utility for parsing CIFAR data
     f = open(fpath, 'rb')
-    d = pickle.load(f)
+    d = pickle.load(f, encoding='latin1')
     f.close()
     data = d['data']
     labels = d[label_key]
@@ -37,17 +64,22 @@ def load_batch(fpath, label_key='labels'):
     data = data.reshape(data.shape[0], 3, 32, 32)
     return data, labels
 
-def show_output(net,testing=False):
+
+def show_output(net, testing=False):
     global error
     global error_divisor
 
     if testing:
         time.sleep(TRAINING_DURATION/2)
         output = net.get_output()
-        output_init = output # Only different line
+        output_init = output  # Only different line
         output = [round(x*255) for x in output]
-        #print "Red: " + str(output[2]) + "\t" + "Green: " + str(output[1]) + "\t" + "Blue: " + str(output[0]) + "\r",
-        #sys.stdout.flush()
+        # print("Red: {0}\tGreen: {1}\tBlue: {2}\r".format(
+        #     str(output[2]),
+        #     str(output[1]),
+        #     str(output[0])
+        # ))
+        # sys.stdout.flush()
         output = np.full((32, 32, 3), output, dtype='uint8')
         cv2.imshow("Output", output)
         cv2.waitKey(int(1000 * TRAINING_DURATION / 2))
@@ -59,8 +91,12 @@ def show_output(net,testing=False):
         time.sleep(TRAINING_DURATION/2)
         output = net.get_output()
         output = [round(x*255) for x in output]
-        #print "Red: " + str(output[2]) + "\t" + "Green: " + str(output[1]) + "\t" + "Blue: " + str(output[0]) + "\r",
-        #sys.stdout.flush()
+        # print("Red: {0}\tGreen: {1}\tBlue: {2}\r".format(
+        #     str(output[2]),
+        #     str(output[1]),
+        #     str(output[0])
+        # ))
+        # sys.stdout.flush()
         output = np.full((32, 32, 3), output, dtype='uint8')
         cv2.imshow("Output", output)
         cv2.waitKey(int(1000 * TRAINING_DURATION / 2))
@@ -69,8 +105,14 @@ def show_output(net,testing=False):
 print("\n___ PLEXUS NETWORK CATDOG EXAMPLE ___\n")
 
 print("Load CIFAR-10 dataset")
-print("Pick random " + str(TRAINING_SAMPLE_SIZE) + " cat and " + str(TRAINING_SAMPLE_SIZE) + " dog images from the CIFAR-10 data batch to TRAIN the network")
-print("Pick random " + str(TESTING_SAMPLE_SIZE) + " cat and " + str(TESTING_SAMPLE_SIZE) + " dog images from the CIFAR-10 test batch to TEST the network")
+print("Pick random {0} cat and {0} dog images from the CIFAR-10 data batch to\
+TRAIN the network".format(
+    str(TRAINING_SAMPLE_SIZE)
+))
+print("Pick random {0} cat and {0} dog images from the CIFAR-10 test batch to\
+TEST the network".format(
+    str(TESTING_SAMPLE_SIZE)
+))
 
 # Load CIFAR-10 dataset
 path = './examples/cifar-10-batches-py/'
@@ -109,12 +151,12 @@ for i in range(0, num_train_samples - 1):
 
 # Generate the testing data
 test_cats = []
-for i in range(0, num_train_samples/5 - 1):
+for i in range(0, int(num_train_samples/5 - 1)):
     if y_test[i] == 3:
         test_cats.append(x_test[i])
 
 test_dogs = []
-for i in range(0, num_train_samples/5 - 1):
+for i in range(0, int(num_train_samples/5 - 1)):
     if y_test[i] == 5:
         test_dogs.append(x_test[i])
 
@@ -124,52 +166,86 @@ red = np.array([0, 0, 255])
 blue_normalized = np.true_divide(blue, 255)
 red_normalized = np.true_divide(red, 255)
 
-print("Create a Plexus network with " + str(SIZE) + " neurons, " + str(INPUT_SIZE) + " of them sensory, " + str(OUTPUT_SIZE) + " of them motor, " + str(CONNECTIVITY) + " connectivity rate, " + str(PRECISION) + " digit precision")
-net = plexus.Network(SIZE,INPUT_SIZE,OUTPUT_SIZE,CONNECTIVITY,PRECISION,RANDOMLY_FIRE,DYNAMIC_OUTPUT,VISUALIZATION)
+print("Create a Plexus network with {0} neurons, {1} of them sensory, {2} of\
+them motor, {3} connectivity rate, {4} digit precision".format(
+    str(SIZE),
+    str(INPUT_SIZE),
+    str(OUTPUT_SIZE),
+    str(CONNECTIVITY),
+    str(PRECISION)
+))
+
+net = plexus.Network(
+    SIZE,
+    INPUT_SIZE,
+    OUTPUT_SIZE,
+    CONNECTIVITY,
+    PRECISION,
+    RANDOMLY_FIRE,
+    DYNAMIC_OUTPUT,
+    VISUALIZATION
+)
 
 print("\n*** LEARNING ***")
 
-print("\nMap " + str(TRAINING_SAMPLE_SIZE/2) + " Different Cat Images to Color Blue & " + str(TRAINING_SAMPLE_SIZE/2) + " Different Dog Images to Color Red - Training Duration: " + str(TRAINING_DURATION * TRAINING_SAMPLE_SIZE) + " seconds (OpenCV latency not included)")
-for i in range(1,TRAINING_SAMPLE_SIZE):
+print("\nMap {0} Different Cat Images to Color Blue & {0} Different Dog Images\
+to Color Red - Training Duration: {1} seconds (OpenCV latency not\
+included)".format(
+    str(TRAINING_SAMPLE_SIZE/2),
+    str(TRAINING_DURATION * TRAINING_SAMPLE_SIZE)
+))
+
+for i in range(1, TRAINING_SAMPLE_SIZE):
     if (i % 2) == 0:
         cat = random.sample(cats, 1)[0]
         cat_normalized = np.true_divide(cat, 255).flatten()
         blue_normalized = np.true_divide(blue, 255).flatten()
         cv2.imshow("Input", cat)
-        net.load(cat_normalized,blue_normalized)
+        net.load(cat_normalized, blue_normalized)
     else:
         dog = random.sample(dogs, 1)[0]
         dog_normalized = np.true_divide(dog, 255).flatten()
         red_normalized = np.true_divide(red, 255).flatten()
         cv2.imshow("Input", dog)
-        net.load(dog_normalized,red_normalized)
+        net.load(dog_normalized, red_normalized)
     show_output(net)
 
 
-print("\nTest " + str(TESTING_SAMPLE_SIZE/2) + " Different Cat Images & " + str(TESTING_SAMPLE_SIZE/2) + " Different Dog Images - Testing Duration: " + str(TRAINING_DURATION * TESTING_SAMPLE_SIZE) + " seconds (OpenCV latency not included)")
-for i in range(1,TESTING_SAMPLE_SIZE):
-    binary_random = random.randint(0,1)
+print("\nTest {0} Different Cat Images & {0} Different Dog Images\
+- Testing Duration: {1} seconds (OpenCV latency not included)".format(
+    str(TRAINING_SAMPLE_SIZE/2),
+    str(TRAINING_DURATION * TESTING_SAMPLE_SIZE)
+))
+
+for i in range(1, TESTING_SAMPLE_SIZE):
+    binary_random = random.randint(0, 1)
     if binary_random == 0:
         cat = random.sample(test_cats, 1)[0]
         cat_normalized = np.true_divide(cat, 255).flatten()
         cv2.imshow("Input", cat)
         net.load(cat_normalized)
-        show_output(net,[1.0, 0.0, 0.0])
+        show_output(net, [1.0, 0.0, 0.0])
     else:
         dog = random.sample(test_dogs, 1)[0]
         dog_normalized = np.true_divide(dog, 255).flatten()
         cv2.imshow("Input", dog)
         net.load(dog_normalized)
-        show_output(net,[0.0, 0.0, 1.0])
+        show_output(net, [0.0, 0.0, 1.0])
 
 
 net.freeze()
 cv2.destroyAllWindows()
 
-print("\n" + str(net.wave_counter) + " waves are executed throughout the network")
+print("\n{0} waves are executed throughout the network".format(
+    str(net.wave_counter)
+))
 
-print("\nIn total: " + str(net.fire_counter) + " times a random non-sensory neuron fired\n")
+print("\nIn total: {0} times a random non-sensory neuron fired\n".format(
+    str(net.fire_counter)
+))
 
-print("\nOverall error: " + str(error/error_divisor) + "\n")
+print("\nOverall error: {0}\n".format(
+    str(error/error_divisor)
+))
 
 print("Exit the program")
--- examples/sequence_basic.py
+++ examples/sequence_basic.py
@@ -1,7 +1,7 @@
 import plexus
 import time
 from itertools import repeat
-import random
+
 
 SIZE = 14
 INPUT_SIZE = 4
@@ -19,8 +19,10 @@ VISUALIZATION = True
 TRAINING_SAMPLE_SIZE = 20
 TESTING_SAMPLE_SIZE = 20
 
+
 def shift(seq):
     return seq[-1:] + seq[:-1]
+
 
 def generate_dataset():
     generated_dataset = []
@@ -36,26 +38,57 @@ def generate_dataset():
     return generated_dataset
 
 
+def notify_the_load(generated_list, output, training_duration):
+    print("Load Input: {0}\tOutput: {1}\tand wait {2} seconds".format(
+        str(generated_list),
+        str(output),
+        str(training_duration)
+    ))
+
 
 print("\n___ PLEXUS NETWORK BASIC SEQUENCE RECOGNITION EXAMPLE ___\n")
 
-print("Create a Plexus network with " + str(SIZE) + " neurons, " + str(INPUT_SIZE) + " of them sensory, " + str(OUTPUT_SIZE) + " of them motor, " + str(CONNECTIVITY) + " connectivity rate, " + str(PRECISION) + " digit precision")
-net = plexus.Network(SIZE,INPUT_SIZE,OUTPUT_SIZE,CONNECTIVITY,PRECISION,RANDOMLY_FIRE,DYNAMIC_OUTPUT,VISUALIZATION)
+print("Create a Plexus network with {0} neurons, {1} of them sensory, {2} of\
+them motor, {3} connectivity rate, {4} digit precision".format(
+    str(SIZE),
+    str(INPUT_SIZE),
+    str(OUTPUT_SIZE),
+    str(CONNECTIVITY),
+    str(PRECISION)
+))
+
+net = plexus.Network(
+    SIZE,
+    INPUT_SIZE,
+    OUTPUT_SIZE,
+    CONNECTIVITY,
+    PRECISION,
+    RANDOMLY_FIRE,
+    DYNAMIC_OUTPUT,
+    VISUALIZATION
+)
 
 print("\n*** LEARNING ***")
 
-print("\nGenerate The Dataset (" + str(TRAINING_SAMPLE_SIZE) + " Items Long) To Recognize a Sequence & Learn for " + str(TRAINING_DURATION) + " Seconds Each")
-for i in range(1,TRAINING_SAMPLE_SIZE):
+print("\nGenerate The Dataset ({0} Items Long) To Recognize a Sequence & Learn\
+for {1} Seconds Each".format(
+    str(TRAINING_SAMPLE_SIZE),
+    str(TRAINING_DURATION)
+))
+
+for i in range(1, TRAINING_SAMPLE_SIZE):
     dataset = generate_dataset()
     for data in dataset:
-        print("Load Input: " + str(data) + "\tand wait " + str(TRAINING_DURATION) + " seconds")
+        notify_the_load(data[0], data[1], TRAINING_DURATION)
         net.load(data[0], data[1])
         time.sleep(TRAINING_DURATION)
 
 
 print("\n\n*** TESTING ***")
 
-print("\nTest the network with random data (" + str(TESTING_SAMPLE_SIZE) + " times)")
+print("\nTest the network with random data ({0} times)".format(
+    str(TESTING_SAMPLE_SIZE)
+))
 error = 0
 error_divisor = 0
 for i in repeat(None, TESTING_SAMPLE_SIZE):
@@ -75,11 +108,17 @@ print("\n")
 net.freeze()
 
 
-print("\n" + str(net.wave_counter) + " waves are executed throughout the network")
+print("\n{0} waves are executed throughout the network".format(
+    str(net.wave_counter)
+))
 
-print("\nIn total: " + str(net.fire_counter) + " times a random non-sensory neuron fired\n")
+print("\nIn total: {0} times a random non-sensory neuron fired\n".format(
+    str(net.fire_counter)
+))
 
 error = error / error_divisor
-print("\nOverall error: " + str(error) + "\n")
+print("\nOverall error: {0}\n".format(
+    str(error)
+))
 
 print("Exit the program")
