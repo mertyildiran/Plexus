9fbf005e47ce8427083e22cac353fcab3b176195
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2019-10-19 12:55:45 +0300

    Fix the linting issues in the other examples in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -15,93 +15,94 @@ here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-	long_description = f.read()
+    long_description = f.read()
 
 setup(
-	name='plexus',
+    name='plexus',
 
-	# Versions should comply with PEP440.  For a discussion on single-sourcing
-	# the version across setup.py and the project code, see
-	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.0.4',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.4',
 
-	description='Plexus is exceptionally bio-inspired, a revolutionary approach to the artificial neural networks',
-	long_description=long_description,
+    description='Plexus is exceptionally bio-inspired, a revolutionary\
+approach to the artificial neural networks',
+    long_description=long_description,
 
-	# The project's main homepage.
-	url='https://github.com/mertyildiran/Plexus',
+    # The project's main homepage.
+    url='https://github.com/mertyildiran/Plexus',
 
-	# Author details
-	author='Mehmet Mert Yildiran',
-	author_email='mert.yildiran@bil.omu.edu.tr',
+    # Author details
+    author='Mehmet Mert Yildiran',
+    author_email='mert.yildiran@bil.omu.edu.tr',
 
-	# Choose your license
-	license='MIT',
+    # Choose your license
+    license='MIT',
 
-	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-	classifiers=[
-		# How mature is this project? Common values are
-		#   3 - Alpha
-		#   4 - Beta
-		#   5 - Production/Stable
-		'Development Status :: 3 - Alpha',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-		# Indicate who your project is intended for
-		'Intended Audience :: Developers',
-		'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
 
-		# Pick your license as you wish (should match "license" above)
-		'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
 
-		# Specify the Python versions you support here. In particular, ensure
-		# that you indicate whether you support Python 2, Python 3 or both.
-		'Programming Language :: Python :: 2.7',
-	],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
 
-	# What does your project relate to?
-	keywords='artifical neural networks',
+    # What does your project relate to?
+    keywords='artifical neural networks',
 
-	# You can just specify the packages manually here if your project is
-	# simple. Or you can use find_packages().
-	packages=['plexus'],
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=['plexus'],
 
-	# Alternatively, if you want to distribute just a my_module.py, uncomment
-	# this:
-	#   py_modules=["my_module"],
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
 
-	# List run-time dependencies here.  These will be installed by pip when
-	# your project is installed. For an analysis of "install_requires" vs pip's
-	# requirements files see:
-	# https://packaging.python.org/en/latest/requirements.html
-	install_requires=[],
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
 
-	# List additional groups of dependencies here (e.g. development
-	# dependencies). You can install these using the following syntax,
-	# for example:
-	# $ pip install -e .[dev,test]
-	extras_require={},
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={},
 
-	# If there are data files included in your packages that need to be
-	# installed, specify them here.  If using Python 2.6 or less, then these
-	# have to be included in MANIFEST.in as well.
-	package_data={},
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
 
-	# Although 'package_data' is the preferred approach, in some case you may
-	# need to place data files outside of your packages. See:
-	# http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-	data_files=[],
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
 
-	# To provide executable scripts, use entry points in preference to the
-	# "scripts" keyword. Entry points provide cross-platform support and allow
-	# pip to create the appropriate form of executable for the target platform.
-	entry_points={},
-	ext_modules=[
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={},
+    ext_modules=[
         Extension(
-			name = 'cplexus',
-			sources = ['plexus/plexus.cpp', 'plexus/wrapper.cpp'],
-			language = 'c++',
-			extra_compile_args = ['-w'],
-		)
+            name='cplexus',
+            sources=['plexus/plexus.cpp', 'plexus/wrapper.cpp'],
+            language='c++',
+            extra_compile_args=['-w'],
+        )
     ]
 )
